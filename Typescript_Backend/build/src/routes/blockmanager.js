4b0511c216bbf23f42cbf04df5dfc75a8a4384f6
Luke Harvey <Lukeharvey0226@gmail.com>
2020-02-13 19:12:32 -0800

    Should output to file after everything

--- Typescript_Backend/build/src/routes/blockmanager.js
+++ Typescript_Backend/build/src/routes/blockmanager.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
 async function blockmanager(revs, pageId) {
     const bl = new Block(pageId, revs, revs.length);
+    JSON.stringify(bl);
+    fs.writeFile('output.json', JSON.stringify(bl), 'utf8', err => {
+        if (err) {
+            console.log('An error occured while writing JSON Object to File.');
+            return console.log(err);
+        }
+        console.log('JSON file has been saved.');
+    });
 }
 exports.blockmanager = blockmanager;
 class Block {
--- Typescript_Backend/src/routes/blockmanager.ts
+++ Typescript_Backend/src/routes/blockmanager.ts
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+
 export async function blockmanager(revs: Revision[], pageId: number) {
   const bl = new Block(pageId, revs, revs.length);
+  JSON.stringify(bl);
+  fs.writeFile('output.json', JSON.stringify(bl), 'utf8', err => {
+    if (err) {
+      console.log('An error occured while writing JSON Object to File.');
+      return console.log(err);
+    }
+    console.log('JSON file has been saved.');
+  });
 }
 
 export class Block {
